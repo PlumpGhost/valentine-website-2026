485072c018d1d1e7a5f3582b6ebfe7d561f81078
PlumpGhost <jordan.lucido@gmail.com>
2026-02-01 13:59:03 +1000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=c-g-UqiIFTQ", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dljz6iz3j/video/upload/v1769918222/chevy_-_if_i_could_ride_a_bike_w_park_bird_wksqc2.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
